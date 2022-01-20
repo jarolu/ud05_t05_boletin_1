034a7222172f60f543a02317b29f4d304bd6763e
Luis Janeiro <jarolu@gmail.com>
2022-01-20 12:48:16 +0100

    Exercicio 3 completo

--- exercicio3.php
+++ exercicio3.php
@@ -7,6 +7,35 @@
     <title>Exercicio 3</title>
 </head>
 <body>
+
+<?php    
+
+
+    $diccionario = array(
+        'table' => 'mesa',
+        'chair' => 'silla',
+        'sofa' => 'sofá',
+        'door' => 'puerta',
+        'window' => 'ventana',
+        'lamp' => 'lámpara',
+        'kitchen' => 'cocina',
+        'fridge' => 'nevera',
+        'dish' => 'plato',
+        'fork' => 'tenedor',
+    );
+
+    echo "<table border='1'>";
+
+    // Recorremos o array asociativo para imprimir cada fila da
+    // táboa;
+    
+    foreach ( $diccionario as $ingles => $castelan ){
+        echo "<tr><td>".$ingles."</td><td>".$castelan."</td></tr>";
+    }
+
+    echo "</table>";
+
+?>
     
 </body>
 </html>
